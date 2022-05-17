1cc50ff35648e9cb2b29c93a4237b98e27a646d0
iudexgundir <govoroverx@icloud.com>
2022-05-17 23:56:13 +0300

    model setup

--- ResinTimerApp/ViewModel/ResinTimerModel.swift
+++ ResinTimerApp/ViewModel/ResinTimerModel.swift
@@ -9,6 +9,12 @@ import SwiftUI
 
 class ResinTimerModel: NSObject, ObservableObject {
     // MARK: Timer Properties
-   
+    @Published var progress: CGFloat = 1
+    @Published var timerStringValue: String = "00:00"
+    @Published var isStarted: Bool = false
+    
+    @Published var hour: Int = 0
+    @Published var minute: Int = 0
+    @Published var seconds: Int = 0
 }
 
